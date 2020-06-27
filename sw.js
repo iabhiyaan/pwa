ee44178482a3433a7a4888eadbdfa8536ff5fc4c
iabhiyaan <iabhiyaan@gmail.com>
2020-06-27 20:50:12 +0545

    Caching assets in sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,34 @@
+const STATIC_CACHE_NAME = "site-static";
+const ASSETS = [
+	"/",
+	"/index.html",
+	"/js/app.js",
+	"/js/ui.js",
+	"/js/materialize.min.js",
+	"/css/styles.css",
+	"/css/materialize.min.css",
+	"/img/dish.png",
+	"https://fonts.googleapis.com/icon?family=Material+Icons",
+	"https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2"
+];
+
 // install service worker
 self.addEventListener("install", (e) => {
-	console.log("Service worker has been installed");
+	// console.log("Service worker has been installed");
+	e.waitUntil(
+		caches.open(STATIC_CACHE_NAME).then((cache) => {
+			console.log("Caching all the assets");
+			cache.addAll(ASSETS);
+		})
+	);
 });
 
 // activate service worker
 self.addEventListener("activate", (e) => {
-	console.log("Service worker has been activated");
+	// console.log("Service worker has been activated");
 });
 
 // fetch service worker
 self.addEventListener("fetch", (e) => {
-	console.log("Service worker has been fetched", e);
+	// console.log("Service worker has been fetched", e);
 });
