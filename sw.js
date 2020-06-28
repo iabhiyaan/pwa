3c5a9cae407b593c330c4dd0035c5eb2d1f79b9f
iabhiyaan <iabhiyaan@gmail.com>
2020-06-28 19:19:22 +0545

    Old cache keys will be deleted and current cache keys will be saved

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const STATIC_CACHE_NAME = "site-static";
+const STATIC_CACHE_NAME = "site-static-v5";
 const ASSETS = [
 	"/",
 	"/index.html",
@@ -14,10 +14,9 @@ const ASSETS = [
 
 // install service worker
 self.addEventListener("install", (e) => {
-	// console.log("Service worker has been installed");
 	e.waitUntil(
 		caches.open(STATIC_CACHE_NAME).then((cache) => {
-			console.log("Caching all the assets");
+			console.log("Installing and Caching all the assets");
 			cache.addAll(ASSETS);
 		})
 	);
@@ -25,7 +24,22 @@ self.addEventListener("install", (e) => {
 
 // activate service worker
 self.addEventListener("activate", (e) => {
-	// console.log("Service worker has been activated");
+	console.log("Activating the app without skiping");
+	e.waitUntil(
+		caches.keys().then((keys) => {
+			return Promise.all(
+				keys
+					.filter((key) => {
+						// These will return all the old cache_keys
+						return key != STATIC_CACHE_NAME;
+					})
+					.map((key) => {
+						// From that old caches it will loop through them and delete it from `caches` object
+						return caches.delete(key);
+					})
+			);
+		})
+	);
 });
 
 // fetch service worker
