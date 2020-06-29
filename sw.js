a25370852c99d61944894a2f02f4a4d4ca905f7a
iabhiyaan <iabhiyaan@gmail.com>
2020-06-29 08:48:54 +0545

    Dynamic caching and limiting cache

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const STATIC_CACHE_NAME = "site-static-v5";
+const DYNAMIC_CACHE_NAME = "site-dynamic-v1";
 const ASSETS = [
 	"/",
 	"/index.html",
@@ -9,8 +10,21 @@ const ASSETS = [
 	"/css/materialize.min.css",
 	"/img/dish.png",
 	"https://fonts.googleapis.com/icon?family=Material+Icons",
-	"https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2"
+	"https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
+	"/pages/fallback.html"
 ];
+
+// cache size limit
+
+function limitCacheSize(name, size) {
+	caches.open(name).then((cache) => {
+		cache.keys().then((keys) => {
+			if (keys.length > size) {
+				cache.delete(keys[0]).then(limitCacheSize(name, size));
+			}
+		});
+	});
+}
 
 // install service worker
 self.addEventListener("install", (e) => {
@@ -31,7 +45,7 @@ self.addEventListener("activate", (e) => {
 				keys
 					.filter((key) => {
 						// These will return all the old cache_keys
-						return key != STATIC_CACHE_NAME;
+						return key != STATIC_CACHE_NAME && key != DYNAMIC_CACHE_NAME;
 					})
 					.map((key) => {
 						// From that old caches it will loop through them and delete it from `caches` object
@@ -49,8 +63,17 @@ self.addEventListener("fetch", (e) => {
 		caches
 			.match(e.request)
 			.then((cacheRes) => {
-				return cacheRes || fetch(e.request);
+				return (
+					cacheRes ||
+					fetch(e.request).then((fetchRes) => {
+						return caches.open(DYNAMIC_CACHE_NAME).then((cache) => {
+							cache.put(e.request.url, fetchRes.clone());
+							limitCacheSize(DYNAMIC_CACHE_NAME, 15);
+							return fetchRes;
+						});
+					})
+				);
 			})
-			.catch()
+			.catch(() => e.request.url.indexOf(".html") > -1 && caches.match("/pages/fallback.html"))
 	);
 });
